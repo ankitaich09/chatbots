22dc023fa79cfab9771b54d5bb4b8e1962cc8c12
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2024-04-16 13:47:50 -0400

    added search by alias

--- sandbox.py
+++ sandbox.py
@@ -4,4 +4,24 @@ import json
 with open('drugs.json', 'r') as jf:
     data = json.load(jf)
 
-print(data.keys())
+
+def find_alias_match(input_string, json_file_path):
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+
+    for item in data.keys():
+        if "aliases" in data[item] and input_string in data[item]["aliases"]:
+            return item
+
+    return None  # Return None if no match is found
+
+
+# Example usage
+input_string = "2ai"
+json_file_path = "drugs.json"
+result = find_alias_match(input_string, json_file_path)
+
+if result:
+    print(result)
+else:
+    print("No match found")
