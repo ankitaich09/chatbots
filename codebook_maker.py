0f935d1a8f2e95de9963a10faf6ee7c7a2993ac0
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2024-04-23 11:02:41 -0400

    Update codebook_maker.py

---
+++ codebook_maker.py
@@ -0,0 +1,25 @@
+import json
+
+#global
+
+with open('drugs.json', 'r') as jfp:
+    data = json.load(jfp)
+
+def get_all_categories():
+    all_cats = []
+
+    for each_drug in data.keys():
+        try:
+            for each_c in data[each_drug]['categories']:
+                all_cats.append(each_c)
+        except:
+            continue
+
+    cats = list(set(all_cats))
+    return cats
+
+
+for each_drug in data.keys()
+    official_name = each_drug
+    aliases = data[each_drug]['aliases']
+    categories = data[each_drug]['categories']
