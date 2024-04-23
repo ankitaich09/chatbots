5c4b7f7e4afae70edcdb818de675930079d1c73f
Ankit Aich <31042284+ankitaich09@users.noreply.github.com>
2024-04-23 09:24:50 -0400

    added search by name

--- sandbox.py
+++ sandbox.py
@@ -20,6 +20,24 @@ def find_alias_match(input_string, json_file_path):
 
     return None  # Return None if no match is found
 
+def find_name_match(input_string, json_file_path):
+    with open(json_file_path, 'r') as file:
+        data = json.load(file)
+
+    for item in data.keys():
+        if input_string == item:
+            temp = {
+                'Name':item,
+                'Aliases': data[item]["aliases"],
+                'Dosage':data[item]["properties"]["dose"],
+                'Summary':data[item]["properties"]["summary"]
+            }
+            return temp
+
+    return None  # Return None if no match is found
+
+
+
 def print_format( input_string, result):
     string = f"You wanted to search for {input_string}. This substance is officially called {result['Name']}. \n The safe dosage is {result['Dosage']} \n. Here's more about the substance :\n {result['Summary']}"
     return string
